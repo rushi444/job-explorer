6e4b6a87ffcb0a6c01209da28915ab2e8f1e63cf
rushi444 <rushi44@gmail.com>
2019-12-04 23:36:27 -0800

    some styling, and danger html added

--- job-explorer/src/components/Dashboard.js
+++ job-explorer/src/components/Dashboard.js
@@ -8,14 +8,19 @@ export class Dashboard extends Component {
     this.props.getAllJobs();
   }
 
- 
-  render() { 
-    return <div>
-        <h1>dashboard</h1>
-        {this.props.jobs && this.props.jobs.map(job => (
-            <Job job={job} key={job.id}/>
-        ))}
-    </div>;
+  render() {
+    return (
+      <div className='dashboardContainer'>
+        <div className='marketMetricsContainer'>
+          <h2>Market Metrics</h2>
+        </div>
+        <div className='jobsContainer'>
+          <h2>Jobs</h2>
+          {this.props.jobs &&
+            this.props.jobs.map(job => <Job job={job} key={job.id} />)}
+        </div>
+      </div>
+    );
   }
 }
 
--- job-explorer/src/components/Job.js
+++ job-explorer/src/components/Job.js
@@ -1,14 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-export class Job extends Component {
-    render() {
-        console.log(this.props)
-        return (
-            <div>
-                <h3>{this.props.job.company}</h3>
-            </div>
-        )
-    }
+class Job extends Component {
+  rawMarkup() {
+    let content = this.props.job.description;
+    return { __html: content };
+  }
+  render() {
+    const dateArray = this.props.job.created_at.split(' ');
+    console.log(this.props.job.description);
+    return (
+      <div>
+        <div className='jobTitleAndLocation'>
+          <div className='title'>
+            <h3>{this.props.job.title}</h3>
+          </div>
+          <div className='location'>
+            <h3>{this.props.job.location}</h3>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h5>{this.props.job.company}</h5>
+            <h5>{this.props.job.type}</h5>
+            <h5>Posted On: {`${dateArray[1]} ${dateArray[2]}`}</h5>
+          </div>
+          <div>
+            <p dangerouslySetInnerHTML={this.rawMarkup()} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Job
+export default Job;
